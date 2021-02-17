e9b512895a2f6f6d2f28c2db06f45972b56351ee
the38th <step_al_ul@mail.ru>
2021-02-17 03:54:39 -0800

    function renamed

--- task1/task1.php
+++ task1/task1.php
@@ -1,11 +1,11 @@
 <?php
-  function searchId($categories, $id) {
+  function searchCategory($categories, $id) {
     if (array_key_exists("id", $categories) && ($categories["id"] == $id)) {
         return $categories["title"];
     }
     foreach ($categories as $category) {
       if (is_array($category)) {
-          return searchId($category, $id);
+          return searchCategory($category, $id);
       }
     }
     return "";
