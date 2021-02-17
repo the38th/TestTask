b872449fd227e2efea0fcba83b6b2fcd579e36a4
the38th <step_al_ul@mail.ru>
2021-02-17 12:54:12 -0800

    fixed task 1

--- task1/task1.php
+++ task1/task1.php
@@ -1,13 +1,18 @@
 <?php
+
   function searchCategory($categories, $id) {
     if (array_key_exists("id", $categories) && ($categories["id"] == $id)) {
         return $categories["title"];
     }
     foreach ($categories as $category) {
       if (is_array($category)) {
-          return searchCategory($category, $id);
+          $temp = searchCategory($category, $id);
+          if ($temp != "") {
+            return $temp;
+          }
       }
     }
     return "";
   }
+  
 ?>
